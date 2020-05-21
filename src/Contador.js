6e3c058e9d4efbfd07479e43f722b5463ce10ab4
duvancorrea96 <duvancorrea96@gmail.com>
2020-05-20 20:33:27 -0500

    using forms 2

--- src/Contador.js
+++ src/Contador.js
@@ -1,31 +1,51 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Contador = () => {
   const { register, error, handleSubmit } = useForm();
 
-  const showData = (data, e) => {
-    console.log(data);
+  const onSend = (data, e) => {
+    setEntrance([...Entrance, data]);
+    e.target.reset();
   };
+
+  const [Entrance, setEntrance] = useState([]);
 
   return (
     <Fragment>
-      <form onSubmit={handleSubmit(showData)}>
+      <h1>Example #1</h1>
+      <form onSubmit={handleSubmit(onSend)}>
         <input
-          name="input"
+          name="title"
+          placeholder="title"
           ref={register({
-            required: { value: true, message: "Input obligatorio" },
+            required: { value: true, message: "title require" },
           })}
         ></input>
+        <p>{error?.title?.message}</p>
         <input
-          name="input2"
+          name="description"
+          placeholder="description"
           ref={register({
-            required: { value: true, message: "Input2 obligatorio" },
+            required: { value: true, message: "description require" },
+            minLength: { value: 5, message: "min 5" },
           })}
         ></input>
-        <span>{error?.input?.message}</span>
+        <p>{error?.description?.message}</p>
         <button>Send</button>
       </form>
+      <ul>
+        {Entrance.map((e, i) => (
+          <Fragment>
+            <li key={i}>
+              <strong>Title</strong> {e.title}
+            </li>
+            <p>
+              <strong>Description</strong> {e.description}
+            </p>
+          </Fragment>
+        ))}
+      </ul>
     </Fragment>
   );
 };
