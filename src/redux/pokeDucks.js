11ad3111e7bf0627481b74f8ad76eb889b50c2fb
duvancorrea96 <duvancorrea96@gmail.com>
2020-05-26 05:20:58 -0500

    paginacion con muuucho sueñooo

--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -3,15 +3,24 @@ import axios from "axios";
 // constans
 const initialData = {
   pokes: [],
+  actualPage: 0,
 };
 
 const GET_POKEMONS = "GET_POKEMONS";
+const NEXT_PAGE = "NEXT_PAGE";
+const PREVIUS_PAGE = "PREVIUS_PAGE";
 
 // reducer
 export default function pokeReducer(state = initialData, action) {
   switch (action.type) {
     case "GET_POKEMONS":
       return { ...state, pokes: action.payload };
+
+    case "NEXT_PAGE":
+      return { ...state, pokes: action.payload, actualPage: action.actualPage };
+
+    case "PREVIUS_PAGE":
+      return { ...state, pokes: action.payload, actualPage: action.actualPage };
 
     default:
       return state;
@@ -20,13 +29,47 @@ export default function pokeReducer(state = initialData, action) {
 
 // actions
 export const getPokemosnAction = () => async (dispatch, getState) => {
+  const actualPage = getState().pokemons.actualPage;
+  console.log(actualPage);
+
   try {
     const res = await axios.get(
-      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
+      `https://pokeapi.co/api/v2/pokemon?offset=${actualPage}&limit=20`
     );
     dispatch({
       type: GET_POKEMONS,
       payload: res.data.results,
+      actualPage: actualPage,
     });
   } catch (error) {}
+};
+
+export const nextPage = () => async (dispatch, getState) => {
+  const actualPage = getState().pokemons.actualPage + 20;
+  console.log(actualPage);
+
+  const res = await axios.get(
+    `https://pokeapi.co/api/v2/pokemon?offset=${actualPage}&limit=20`
+  );
+
+  dispatch({
+    type: NEXT_PAGE,
+    payload: res.data.results,
+    actualPage: actualPage,
+  });
+};
+
+export const previousPage = () => async (dispatch, getState) => {
+  const actualPage = getState().pokemons.actualPage - 20;
+  console.log(actualPage);
+
+  const res = await axios.get(
+    `https://pokeapi.co/api/v2/pokemon?offset=${actualPage}&limit=20`
+  );
+
+  dispatch({
+    type: PREVIUS_PAGE,
+    payload: res.data.results,
+    actualPage: actualPage,
+  });
 };
